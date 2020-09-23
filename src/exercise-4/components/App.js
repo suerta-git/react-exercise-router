4a099e5cd3a00ec00bae11c70a7988d2a3f22c51
Suerta <suerta__@hotmail.com>
2020-09-23 19:04:05 +0800

    [Zhang Jindong] feat: impletement exercise-4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import {Route} from "react-router";
 import Home from "./Home";
 import About from "./About";
@@ -11,10 +11,12 @@ class App extends Component {
     return (
       <div className="app">
         <Router>
-            <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
-            <Route path='/about' component={About} />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+              <Route path='/not-exist-path' component={NotMatch} />
+              <Route path='/:user' component={User} />
+            </Switch>
         </Router>
       </div>
     );
